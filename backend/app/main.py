a040e68bb1516d4f03d6842402c48ebe1b059088
aacho04 <aaditinarayan14@gmail.com>
2026-04-29 23:41:55 +0530

    Added Acuracy and f1_Scrore of each model;

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 import pickle
 import pandas as pd
+from ml.train_model import get_model_metrics
+
 
 app = FastAPI()
 
@@ -9,25 +11,74 @@ model = pickle.load(open("ml/models/model.pkl", "rb"))
 scaler = pickle.load(open("ml/models/scaler.pkl", "rb"))
 
 
+
 @app.get("/")
 def home():
     return {"message": "Loan Risk Prediction API is running"}
 
 
+# @app.post("/predict")
+# def predict(data: dict):
+    
+#     # Convert input to DataFrame
+#     df = pd.DataFrame([data])
+
+#     # Scale input
+#     df_scaled = scaler.transform(df)
+
+#     # Predict
+#     prediction = model.predict(df_scaled)[0]
+#     probability = model.predict_proba(df_scaled)[0][1]
+
+#     return {
+#         "prediction": int(prediction),
+#         "probability": float(probability)
+#     }
+
+# @app.post("/predict")
+# def predict(data: dict):
+    
+#     df = pd.DataFrame([data])
+#     df_scaled = scaler.transform(df)
+
+#     prediction = model.predict(df_scaled)[0]
+#     probability = model.predict_proba(df_scaled)[0][1]
+
+#     # 🔥 Convert to human-readable
+#     if prediction == 1:
+#         message = "Loan Approved ✅"
+#     else:
+#         message = "Loan Rejected ❌"
+
+
+#     return {
+#         "message": message,
+#         "probability": round(float(probability), 2)
+#     }
 @app.post("/predict")
 def predict(data: dict):
     
-    # Convert input to DataFrame
     df = pd.DataFrame([data])
-
-    # Scale input
     df_scaled = scaler.transform(df)
 
-    # Predict
+    # Prediction
     prediction = model.predict(df_scaled)[0]
     probability = model.predict_proba(df_scaled)[0][1]
 
+    # Message
+    if prediction == 1:
+        message = "Loan Approved ✅"
+    else:
+        message = "Loan Rejected ❌"
+
+    # 🔥 Get model metrics
+    metrics = get_model_metrics()
+
+    # ✅ Final response
     return {
-        "prediction": int(prediction),
-        "probability": float(probability)
+        "prediction": {
+            "message": message,
+            "probability": round(float(probability), 2)
+        },
+        "model_metrics": metrics
     }
